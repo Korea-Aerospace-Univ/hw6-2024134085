23d7bc813400f882891333a51029864e5d061458
2024134085 <james050527@gmail.com>
2026-05-27 15:51:58 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -15,9 +15,8 @@ int main(void)
         scanf("%d", p);
     }
     
-    for(int *p1 = arr1, *p2 = arr2+N-1; p1 < arr1+N; p1++){
+    for(int *p1 = arr1, *p2 = arr2+N-1; p1 < arr1+N; p1++, p2--){
         printf(" %d", *p1 + *p2);
-        p2--;
     }
 
     return 0;
