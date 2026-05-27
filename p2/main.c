e4c51fa64ff37e179f35c61408ccd7f7b2f7cc80
2024134085 <james050527@gmail.com>
2026-05-27 15:27:43 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -3,10 +3,20 @@
 int main(void)
 {
     int N;
-    int arr1[20], arr2[20];
+    int arr1[20]={}, arr2[20]={};
     
-    for(int *p = arr1; p < arr+N; p++){
-        *p
+    scanf("%d", &N);
+    
+    for(int *p = arr1; p < arr1+N; p++){
+        scanf(" %d", &p);
+    }
+    
+    for(int *p = arr2; p < arr2+N; p++){
+        scanf(" %d", &p);
+    }
+    
+    for(int *p1 = arr1, *p2 = arr2+N-1; p1 < arr1+N; p1++){
+        printf(" %d",*p1 + *p2);
     }
 
     return 0;
